c8b82b8c13d520da9d4452fdb173e1d7d97bed49
Junichi Kakisako <kilo.sw@gmail.com>
2020-06-21 21:27:26 +0900

    Wait for the process to finish

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 mod config;
 
+use std::time;
+use std::thread;
+use sysinfo::{System, SystemExt};
+
 fn main() {
     let config = config::parse_args();
+    let mut threads = vec![];
 
-    println!("Hello, world!");
-    println!("{:?}", config);
+    for &pid in config.pid_list.iter() {
+        threads.push(
+            thread::spawn(move || {
+                let system = System::new_all();
+                let sleep_duration = time::Duration::from_secs(1);
+                loop {
+                    match system.get_process(pid) {
+                        None => {
+                            break;
+                        }
+                        Some(_p) => {
+                        }
+                    }
+                    thread::sleep(sleep_duration);
+                }
+            })
+        );
+    }
+
+    for th in threads {
+        th.join().unwrap();
+    }
 }
